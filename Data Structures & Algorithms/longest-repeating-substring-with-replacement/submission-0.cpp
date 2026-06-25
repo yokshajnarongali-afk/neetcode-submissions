e70eccc5cf22af1cd02f0038412cd3fc1af71189
yokshajnarongali-afk <yokshajnarongali@gmail.com>
2026-06-25 15:39:29 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   int characterReplacement (string s , int k)
+{
+    vector<int> freq (26 , 0) ;
+
+    int left = 0 ;
+    int maxFreq = 0 ;
+    int ans = 0 ;
+
+    for (int right=0 ; right < s.size () ; right++)
+    {
+        freq[s[right] - 'A']++ ;
+
+        maxFreq = max (maxFreq , freq[s[right] - 'A']) ;
+
+        while ((right - left + 1) - maxFreq > k)
+        {
+            freq[s[left] - 'A']-- ;
+            left ++ ;
+        }
+
+        ans = max (ans , right - left + 1) ;
+    }
+
+    return ans ;
+}
+};
